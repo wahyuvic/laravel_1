8d5227afd16d51f066dcb38480a114b655541e1e
wahyuvic <wardiansyah62@gmail.com>
2024-01-21 19:17:52 +0700

    Add files via upload

---
+++ database/migrations/2022_11_16_093810_add_nip_pegawai.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNipPegawai extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pegawai', function (Blueprint $table) {
+            $table->string('gelar')->nullable()->change();
+            $table->string('nip');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
